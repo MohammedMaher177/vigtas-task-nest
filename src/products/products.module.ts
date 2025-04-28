0b0a84c7c2536a7e8c2a10bdc70b4224870e4ca9
Mohammed Maher <mohamed.maher@digi360.tech>
2025-04-28 16:38:24 +0300

    feat: integrate categories module into products module
    This commit integrates the `CategoriesModule` into the `ProductsModule` to enable access to category-related functionalities within the products module.
    
    - Imports `CategoriesModule` into `ProductsModule`.
    - Adds `CategoriesModule` to the `imports` array in the `ProductsModule`'s `@Module` decorator.
    - Exports `CategoriesService` from `CategoriesModule` to make it available for injection in other modules.
    - Imports `TypeOrmModule` and the `Product` entity into `ProductsModule` to configure TypeORM for the `Product` entity.

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entity';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product]), CategoriesModule],
   providers: [ProductsService],
   controllers: [ProductsController]
 })
-export class ProductsModule {}
+export class ProductsModule { }
