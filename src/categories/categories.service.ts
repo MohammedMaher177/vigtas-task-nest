7915d6030ab0f10b8377e5d52826a6cb4f530893
Mohammed Maher <mohamed.maher@digi360.tech>
2025-04-28 16:01:20 +0300

    feat: prevent duplicate category names
    This commit prevents the creation of categories with duplicate names.
    
    - Implements a check in the `create` method of the `CategoriesService` to verify if a category with the given name already exists.
    - Throws a `ConflictException` if a category with the same name is found.
    - Adds a `findByName` method to the `CategoriesService` to search for categories by name, using a case-insensitive query.
    - Imports `ConflictException` from `@nestjs/common`.

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from './entity';
@@ -10,11 +10,26 @@ export class CategoriesService {
   constructor(
     @InjectRepository(Category)
     private categoryRepository: Repository<Category>,
-  ) {}
+  ) { }
 
   async create(createCategoryDto: CreateCategoryDto): Promise<ICategory> {
+
+    const existingCategory = await this.findByName(createCategoryDto.name);
+
+    if (existingCategory) {
+      throw new ConflictException('Category with this name already exists');
+    }
+
+
     const category = this.categoryRepository.create(createCategoryDto);
     return this.categoryRepository.save(category);
+  }
+
+  async findByName(name: string): Promise<ICategory | undefined> {
+    return this.categoryRepository
+      .createQueryBuilder('category')
+      .where('LOWER(category.name) = LOWER(:name)', { name })
+      .getOne();
   }
 
   findAll(): Promise<ICategory[]> {
