d3a5f2efff6473d36be2480405b2da43c28470b3
Mohammed Maher <mohamed.maher@digi360.tech>
2025-04-28 15:59:19 +0300

    feat: add unique constraint to category name
    This commit adds a unique constraint to the `name` column in the `Category` entity.
    
    - Adds the `@Unique(['name'])` decorator to the `Category` entity to ensure that each category has a unique name.

--- src/categories/entity/category.entity.ts
+++ src/categories/entity/category.entity.ts
@@ -1,7 +1,8 @@
 import { Product } from 'src/products/entity';
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, Unique } from 'typeorm';
 
 @Entity('categories')
+@Unique(['name'])
 export class Category {
   @PrimaryGeneratedColumn('uuid')
   id: string; 
